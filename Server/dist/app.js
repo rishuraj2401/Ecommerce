8f2ba9eba979c867ae213b99ee42c652250ab16f
rishuraj2401 <rishuraj2401sinha@gmail.com>
2024-02-08 19:51:53 +0530

    solved2

--- Server/dist/app.js
+++ Server/dist/app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.send("API Working with /api/v1");
 });
 // Using Routes
