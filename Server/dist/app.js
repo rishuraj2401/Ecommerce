4e36baec168693c38972c640adac448aa9f3a599
rishuraj2401 <rishuraj2401sinha@gmail.com>
2024-02-08 16:56:20 +0530

    ver2

--- Server/dist/app.js
+++ Server/dist/app.js
@@ -6,46 +6,36 @@ import { config } from "dotenv";
 import morgan from "morgan";
 import Stripe from "stripe";
 import cors from "cors";
-import path from "path"
 // Importing Routes
 import userRoute from "./routes/user.js";
 import productRoute from "./routes/products.js";
 import orderRoute from "./routes/order.js";
 import paymentRoute from "./routes/payment.js";
 import dashboardRoute from "./routes/stats.js";
-    
 config({
-  path: "./.env",
+    path: "./.env",
 });
-
 const port = process.env.PORT || 4000;
 const mongoURI = process.env.MONGO_URI || "";
 const stripeKey = process.env.STRIPE_KEY || "";
-
 connectDB(mongoURI);
-
-export const stripe = new Stripe(stripeKey);
+export const stripe = new Stripe("sk_test_51OhAXRSBWlMp8QMqpJrILdrxB2zwmArLVKoQU9VWH3lYi85mRjq6DbcIgDR1YZoADZKao3I36TU17kkEOPjD8FXk00Qj5uDL9M");
 export const myCache = new NodeCache();
-
 const app = express();
-
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-
-app.get("*", (req, res) => {
-  res.send("API Working with /api/v1");
+app.get("/", (req, res) => {
+    res.send("API Working with /api/v1");
 });
-
 // Using Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/order", orderRoute);
 app.use("/api/v1/payment", paymentRoute);
 app.use("/api/v1/dashboard", dashboardRoute);
-
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 app.listen(port, () => {
-  console.log(`Express is working on http://localhost:${port}`);
+    console.log(`Express is working on http://localhost:${port}`);
 });
